b48adf2196527f9a76d357026d631793139a1b3e
Michael Hladky <michael@hladky.at>
2017-10-08 12:05:01 +0200

    wip(release scripts): refresh project

--- chore/scripts/tasks/refresh.js
+++ chore/scripts/tasks/refresh.js
@@ -5,29 +5,30 @@ const path = require('path')
 const util = require('util')
 const exec = util.promisify(require('child_process').exec)
 
-const config = require(path.join('..','..', 'config'))
-
+const config = require(path.join('..', '..', 'config'))
 const utils = require(path.join(__base, 'chore', 'scripts', 'utils'))
 
 process.env.DEBUG = config.debugMode
 
-return exec('git pull --rebase', {cwd: __base})
-;
-
-/*new Promise((resolve, reject) => {
-  rimraf(path.join(__base, 'coverage'), function (err) {
+return new Promise((resolve, reject) => {
+  rimraf(path.join(__base, 'node_modules'), function (err) {
     if (err) {
+      console.error('remove files error: ', err)
       reject(err)
     }
-    console.log('remove files done')
+    console.info('remove files done')
     resolve()
   })
 })
-  .then(() => */exec('git pull --rebase', {cwd: __base})//)
   .then(() => {
-    console.log('start npm install');
+    console.info('start git pull --rebase')
+    return exec('git pull --rebase', {cwd: __base})
+  })
+  .then(() => {
+    console.info('start npm install')
     return exec('npm install', {cwd: __base})
   })
   .then(() => {
-    console.log('end npm install');
+    console.info('end npm install')
   })
+
