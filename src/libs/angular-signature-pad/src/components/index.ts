79b4a1a064f65eca1732a017de625a74af3ae4da
Matteo Gaggiano <maxxxx92@gmail.com>
2018-10-08 11:23:40 +0200

    Added indexes file

---
+++ src/libs/angular-signature-pad/src/components/index.ts
@@ -0,0 +1,4 @@
+export * from './signature-pad/signature-pad.component';
+export * from './signature-pad-card/signature-pad-card.component';
+export * from './signature-pad-card-group/signature-pad-card-group.component';
+export * from './signature-pad-control/signature-pad-control.component';
---
+++ src/libs/angular-signature-pad/src/validators/index.ts
@@ -0,0 +1,5 @@
+export * from './consists-of-arrays.validation';
+export * from './consists-of-object.validation';
+export * from './is-array.validation';
+export * from './min-lines.validation';
+export * from './min-points-in-each-n-lines.validator';
